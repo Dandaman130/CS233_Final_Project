dccaf4d3b7495ccc03c67425a5033937f6cf978d
Kevin Sullivan <sullivankevm@gmail.com>
2025-11-30 16:27:20 -0500

    Basic breakdown of pages. Explination of pages is also listed on Discord

---
+++ lib/screens/income.dart
@@ -0,0 +1,18 @@
+/*
+This page will use pretty similar logic to what the expenses.dart file entails.
+The main difference between the two is that this page will track any income
+entries that the user adds. Expenses should take into account the amount of
+income that the user has. If the user has more expenses than income, the value
+should be negative.
+
+Features:
+- Add income
+- Allow for users to edit/delete entries
+- Anything else we can think of
+- (We could add the monthly/categorical charts if we want, but since the main
+feature of the app is tracking expenses, we can add it here only if we feel
+the need to do so.
+ */
+
+import 'package:flutter/material.dart';
+import 'package:fl_chart/fl_chart.dart'; //Package to implement chart
---
+++ lib/screens/settings.dart
@@ -0,0 +1,12 @@
+/*
+This page will act as the settings for the app. Because of our time frame,
+the settings for the app should be basic and not include too many features
+(unless we have time of course)
+
+Features:
+- Set the currency type (Euro, peso, yen, etc.)
+- Dark Mode toggle
+- Date Persistence (Making sure user info is saved after app is closed)
+ */
+
+import 'package:flutter/material.dart';
